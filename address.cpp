2b5a314277876d1d6cc30789f71c356993db04f4
Ubuntu <ubuntu@ip-172-31-57-171.ec2.internal>
2021-06-08 17:51:21 +0000

    edited address.cpp

--- address.cpp
+++ address.cpp
@@ -15,8 +15,8 @@ std::string AddressFormat(std::string name, std::string streetNum,
                           std::string aptNum = "") {
     // this function takes address information from the user and capitalizes
     // every letter while formatting it in Canadian post format with a default
-    // value of None for the apt_num
-    std::string formattedAddress = "";
+    // value of "" for the aptNum
+    std::string formattedAddress = "", formattedAddressUpper = "";
     formattedAddress = formattedAddress + name + "\n";
 
     if (aptNum != "") {
@@ -26,7 +26,17 @@ std::string AddressFormat(std::string name, std::string streetNum,
     formattedAddress = formattedAddress + streetNum + " " + streetNameType
                        + "\n" + city + " " + province + " " + postalCode;
 
-    return formattedAddress;
+    // get the length of the string
+    int formattedAddressLength = formattedAddress.length();
+    // create an empty char array
+    char char_array[formattedAddressLength + 1] = {};
+    // copy the string to the char array letter by letter
+    strcpy(char_array, formattedAddress.c_str());
+    for (int counter = 0; counter < formattedAddressLength; counter++) {
+        formattedAddressUpper += std::toupper(char_array[counter]);
+    }
+
+    return formattedAddressUpper;
 }
 
 
@@ -34,10 +44,7 @@ int main() {
     // define variables
     std::string userName, userApt, userAptNum, userStreetNum;
     std::string userStreetNameType, userCity, userProvince, userPostalCode;
-    std::string userNameUpper = "", userAptNumUpper = "", userCityUpper = "";
-    std::string userStreetNumUpper = "", userStreetNameTypeUpper = "";
-    std::string userProvinceUpper = "", userPostalCodeUpper = "";
-    std::string mailingAddress;
+    std::string mailingAddress = "";
 
     // greet and get the user's address information
     std::cout << "This program formats an address to a Canadian ";
@@ -45,12 +52,6 @@ int main() {
 
     std::cout << "Enter your full name: ";
     getline(std::cin, userName);
-    int userNameLength = userName.length();
-    char name_array[userNameLength + 1];
-    strcpy(name_array, userName.c_str());
-    for (int counter = 0; counter < userNameLength; counter++) {
-        userNameUpper += std::toupper(name_array[counter]);
-    }
 
     std::cout << "Do you live in an apartment? (y/n): ";
     getline(std::cin, userApt);
@@ -59,71 +60,35 @@ int main() {
         // check if the user has an apartment
         std::cout << "Enter your apartment number: ";
         getline(std::cin, userAptNum);
-        int userAptNumLength = userAptNum.length();
-        char aptNum_array[userAptNumLength + 1];
-        strcpy(aptNum_array, userAptNum.c_str());
-        for (int counter = 0; counter < userAptNumLength; counter++) {
-            userAptNumUpper += std::toupper(aptNum_array[counter]);
-        }
     }
 
     std::cout << "Enter your street number: ";
     getline(std::cin, userStreetNum);
-    int userStreetNumLength = userStreetNum.length();
-    char streetNum_array[userStreetNumLength + 1];
-    strcpy(streetNum_array, userStreetNum.c_str());
-    for (int counter = 0; counter < userStreetNumLength; counter++) {
-        userStreetNumUpper += std::toupper(streetNum_array[counter]);
-    }
 
     std::cout << "Enter your street name AND type ";
     std::cout << "(Main St., Sunset Blvd., etc.): ";
     getline(std::cin, userStreetNameType);
-    int userStreetNameTypeLength = userStreetNameType.length();
-    char streetNameType_array[userStreetNameTypeLength + 1];
-    strcpy(streetNameType_array, userStreetNameType.c_str());
-    for (int counter = 0; counter < userStreetNameTypeLength; counter++) {
-        userStreetNameTypeUpper += std::toupper(streetNameType_array[counter]);
-    }
 
     std::cout << "Enter your city: ";
     getline(std::cin, userCity);
-    int userCityLength = userCity.length();
-    char city_array[userCityLength + 1];
-    strcpy(city_array, userCity.c_str());
-    for (int counter = 0; counter < userCityLength; counter++) {
-        userCityUpper += std::toupper(city_array[counter]);
-    }
 
     std::cout << "Enter your province as an abbreviation (ON, QC, etc.): ";
     getline(std::cin, userProvince);
-    int userProvinceLength = userProvince.length();
-    char province_array[userProvinceLength + 1];
-    strcpy(province_array, userProvince.c_str());
-    for (int counter = 0; counter < userProvinceLength; counter++) {
-        userProvinceUpper += std::toupper(province_array[counter]);
-    }
 
     std::cout << "Enter your postal code: ";
     getline(std::cin, userPostalCode);
-    int userPostalCodeLength = userPostalCode.length();
-    char postalCode_array[userPostalCodeLength + 1];
-    strcpy(postalCode_array, userPostalCode.c_str());
-    for (int counter = 0; counter < userPostalCodeLength; counter++) {
-        userPostalCodeUpper += std::toupper(postalCode_array[counter]);
-    }
 
     if (userApt == "y") {
         // call function if the user has an apartment number
-        mailingAddress = AddressFormat(userNameUpper, userStreetNumUpper,
-                                       userStreetNameTypeUpper, userCityUpper,
-                                       userProvinceUpper, userPostalCodeUpper,
-                                       userAptNumUpper);
+        mailingAddress = AddressFormat(userName, userStreetNum,
+                                       userStreetNameType, userCity,
+                                       userProvince, userPostalCode,
+                                       userAptNum);
     } else {
         // call function if the user does not have an apartment number
-        mailingAddress = AddressFormat(userNameUpper, userStreetNumUpper,
-                                       userStreetNameTypeUpper, userCityUpper,
-                                       userProvinceUpper, userPostalCodeUpper);
+        mailingAddress = AddressFormat(userName, userStreetNum,
+                                       userStreetNameType, userCity,
+                                       userProvince, userPostalCode);
     }
 
     std::cout << "\n";
